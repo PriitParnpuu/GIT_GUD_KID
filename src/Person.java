6ba061bb5fd30123c71be0af908e2946f6b192bd
Priit Pärnpuu <iampriit@gmail.com>
2024-11-12 09:51:03 +0200

    Added Person Class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,14 @@
 public class Person {
+
+    private String name = "GitHub";
+    private int age=34;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+        System.out.println(name);
+        System.out.println(age);
+
+    }
 }
